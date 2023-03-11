6556b5b3416343e4d3c3b87e256485b8932b4ac7
kouehema <kankoe-ange-herman.koue-hemazro@etu.u-paris.fr>
2023-03-11 13:38:22 +0100

    debut client

---
+++ main_client.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include "client.h"
+
+#define DEFAULT_SERVER "lulu"
+#define DEFAULT_PORT "7777"
+#define SIZE_MESS 100
+
+int main(int argc, char** argv) {
+
+    char hostname[SIZE_MESS];
+    char port[SIZE_MESS];
+    if (argc < 3) {
+        sprintf(hostname,"%s",DEFAULT_SERVER);
+        sprintf(port,"%s",DEFAULT_PORT);
+    }else{
+        sprintf(hostname,"%s",argv[1]);
+        sprintf(port,"%s",argv[2]);
+    }
+    struct sockaddr_in6* server_addr;
+    int fdsock, adrlen;
+    
+    switch (get_server_addr(hostname,port, &fdsock, &server_addr, &adrlen)) {
+    case 0: printf("adresse creee !\n"); break;
+    case -1:
+      fprintf(stderr, "Erreur: hote non trouve.\n"); 
+    case -2:
+      fprintf(stderr, "Erreur: echec de creation de la socket.\n");
+      exit(1);
+    }
+
+    affiche_adresse(server_addr);
+    
+    return 0;
+}
